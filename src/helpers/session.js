48449a84a961b9af69766e0e22888f6046e9565a
Daniel Lim <daniellim4400@gmail.com>
2021-04-04 13:03:43 +0700

    add session helper

---
+++ src/helpers/session.js
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default class Session {
+    static getValue = async (key) => {
+        try {
+            return await AsyncStorage.getValue(key);
+        } catch (error) {
+            return null;
+        }
+    }
+    static setValue = async (key, value) => {
+        try {
+            await AsyncStorage.setItem(key, value);
+            return value;
+        } catch (error) {
+            return error;
+        }
+    }
+    static removeValue = async (key) => {
+        try {
+            let keyValue = Session.getValue(key);
+            if(keyValue !== null) {
+                await AsyncStorage.removeItem(key);
+                return keyValue;
+            }
+            return null;
+        } catch (error) {
+            return error;
+        }
+    }
+}
