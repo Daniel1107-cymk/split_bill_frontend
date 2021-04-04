fdf6491d5c1fa40d4253ce4cb977183abddfde15
Daniel Lim <daniellim4400@gmail.com>
2021-04-04 22:00:17 +0700

    change token expired time to string

--- src/helpers/tokenHandler.js
+++ src/helpers/tokenHandler.js
@@ -4,13 +4,13 @@ export default class Token {
     static save(token, expire) {
         const tokenExpired = Date.now() + expire * 1000;
         AsyncStorage.setItem("token", token);
-        AsyncStorage.setItem("token_expired", tokenExpired);
+        AsyncStorage.setItem("token_expired", tokenExpired.toString());
     }
   
     static isExpired() {
         const currentTime = Date.now();
         const tokenExpired = AsyncStorage.getItem("token_expired");
-        return +currentTime >= +tokenExpired;
+        return +currentTime >= +tokenExpired.toInt();
     }
   
     static removeToken() {
