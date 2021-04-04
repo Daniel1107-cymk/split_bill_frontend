e7f42662c7bdbca9c4b438b5a6409d603b852c6c
Daniel Lim <daniellim4400@gmail.com>
2021-04-04 22:01:12 +0700

    add login register and home component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 // screens
-import Login from './src/screens/login';
-import Register from './src/screens/register';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
 const Stack = createStackNavigator();
 
@@ -13,19 +13,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" options={{
+          headerShown: false
+        }} component={Login} />
+        <Stack.Screen name="Register" options={{
+          headerShown: false
+        }} component={Register} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
