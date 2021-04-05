7ceb1e0962b65729218307663bac7da698b3d9d0
Daniel Lim <daniellim4400@gmail.com>
2021-04-05 21:39:33 +0700

    bottom tab with login

--- App.js
+++ App.js
@@ -1,27 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// icon
+import IonIcons from '@expo/vector-icons/Ionicons';
 // screens
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator 
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if(route.name === 'Home') {
+            iconName = 'home-outline'
+          } else if (route.name === 'Login') {
+            iconName = 'add-circle-outline';
+          } 
+          else if (route.name === 'Register') {
+            iconName = 'person-circle-outline';
+          }
+          return <IonIcons name={iconName} color={color} size={size} />
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: '#03befc',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="Register" component={Register} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" options={{
-          headerShown: false
-        }} component={Login} />
-        <Stack.Screen name="Register" options={{
-          headerShown: false
-        }} component={Register} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
+      <MyTabs />
     </NavigationContainer>
   );
 }
