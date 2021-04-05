5378716ff2ff15c3a659efbbe75bdccc62c90e06
Daniel Lim <daniellim4400@gmail.com>
2021-04-05 23:03:43 +0700

    fix navigation setting for different screen

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 // icon
 import IonIcons from '@expo/vector-icons/Ionicons';
 // screens
 import Home from './src/screens/Home';
+import Profile from './src/screens/Profile';
+import Split from './src/screens/Split';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
@@ -20,10 +23,10 @@ const MyTabs = () => {
           let iconName;
           if(route.name === 'Home') {
             iconName = 'home-outline'
-          } else if (route.name === 'Login') {
+          } else if (route.name === 'Split') {
             iconName = 'add-circle-outline';
           } 
-          else if (route.name === 'Register') {
+          else if (route.name === 'Profile') {
             iconName = 'person-circle-outline';
           }
           return <IonIcons name={iconName} color={color} size={size} />
@@ -35,8 +38,8 @@ const MyTabs = () => {
       }}
     >
       <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Login" component={Login} />
-      <Tab.Screen name="Register" component={Register} />
+      <Tab.Screen name="Split" component={Split} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   )
 }
@@ -44,7 +47,11 @@ const MyTabs = () => {
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{title: "Split Bill", headerTitleAlign: 'center'}} component={MyTabs} />
+        <Stack.Screen name="Register" options={{headerShown: false}} component={Register} />
+        <Stack.Screen name="Login" options={{headerShown: false}} component={Login} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
