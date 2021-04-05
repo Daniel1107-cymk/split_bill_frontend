4bbccd62df1d287a1e016c45680a3f103ab14ab0
Daniel Lim <daniellim4400@gmail.com>
2021-04-05 22:22:02 +0700

    fix tokenhandler on save and isexpired

--- src/helpers/tokenHandler.js
+++ src/helpers/tokenHandler.js
@@ -4,12 +4,13 @@ export default class Token {
     static async save(token, expire) {
         const tokenExpired = Date.now() + expire * 1000;
         await AsyncStorage.setItem("token", token);
-        await AsyncStorage.setItem("token_expired", tokenExpired);
+        await AsyncStorage.setItem("token_expired", tokenExpired.toString());
     }
   
     static async isExpired() {
         const currentTime = Date.now();
         const tokenExpired = await AsyncStorage.getItem("token_expired");
+        return +currentTime >= +tokenExpired;
     }
   
     static async removeToken() {
