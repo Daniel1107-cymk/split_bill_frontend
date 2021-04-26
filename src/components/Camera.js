8752736cf3e2781e7def69bc70a760315ac06494
Daniel Lim <daniellim4400@gmail.com>
2021-04-26 22:14:48 +0700

    update camera function

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 //icon
@@ -10,6 +10,8 @@ const Cameras = props => {
   const type = Camera.Constants.Type.back;
   const [hasPermission, setHasPermission] = useState(null);
   const [isCameraReady, setIsCameraReady] = useState(false);
+  const [isShowConfirm, setIsShowConfirm] = useState(false);
+  const [ImageUri, setImageUri] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -33,8 +35,8 @@ const Cameras = props => {
     if(cameraRef !== null) {
       if(isCameraReady) {
         const result = await cameraRef.current.takePictureAsync();
-        props.handleTesseractOcr(result.uri);
-        props.handleIsShowCamera();
+        setImageUri(result.uri);
+        setIsShowConfirm(true);
       }
     } else {
       ToastAndroid.show(
@@ -44,15 +46,25 @@ const Cameras = props => {
     }
   }
 
+  const ImageConfirm = () => {
+    props.handleIsShowCamera();
+    props.handleTesseractOcr(ImageUri);
+  }
+
   const pickImage = async () => {
+    ToastAndroid.show(
+      'Sorry, this feature cannot be use in a moment!',
+      ToastAndroid.SHORT
+    );
+    return;
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
     });
     if (!result.cancelled) {
-      props.handleTesseractOcr(result.uri);
+      let uri = result.uri.replace('file:/', '');
       props.handleIsShowCamera();
+      props.handleTesseractOcr(uri);
     }
   };
 
@@ -66,34 +78,54 @@ const Cameras = props => {
 
   return (
     <View style={styles.container}>
-      <Camera 
-        style={styles.camera} 
-        type={type} 
-        ratio={"16:9"} 
-        onCameraReady={handleOnCameraReady}
-        ref={cameraRef}
-      >
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={[styles.button, {backgroundColor: 'white'}]}
-            onPress={pickImage}
+      {isShowConfirm
+        ? <View style={styles.camera}>
+            <Image source={{uri: ImageUri}} style={styles.imageConfirm} />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: 'transparent'}]}
+                onPress={() => setIsShowConfirm(false)}
+              >
+                <IonIcons name={"close-circle-outline"} color="#fff" size={60} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: 'transparent'}]}
+                onPress={ImageConfirm}
+              >
+                <IonIcons name={"checkmark-circle-outline"} color="#fff" size={60} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        : <Camera 
+            style={styles.camera} 
+            type={type} 
+            ratio={"16:9"} 
+            onCameraReady={handleOnCameraReady}
+            ref={cameraRef}
+            flashMode="off"
           >
-            <IonIcons name={"image-outline"} color="#000" size={30} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.captureButton}
-            onPress={captureImage}
-          >
-            <Text>Capture</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.button, {backgroundColor: 'transparent'}]}
-            onPress={props.handleIsShowCamera}
-          >
-            <IonIcons name={"close-circle-outline"} color="#fff" size={60} />
-          </TouchableOpacity>
-        </View>
-      </Camera>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: 'white'}]}
+                onPress={pickImage}
+              >
+                <IonIcons name={"image-outline"} color="#000" size={30} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.captureButton}
+                onPress={captureImage}
+              >
+                <Text>Capture</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: 'transparent'}]}
+                onPress={props.handleIsShowCamera}
+              >
+                <IonIcons name={"close-circle-outline"} color="#fff" size={60} />
+              </TouchableOpacity>
+            </View>
+          </Camera>
+      }
     </View>
   );
 }
@@ -107,8 +139,15 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-end',
   },
+  imageConfirm: {
+    flex: 1,
+  },
   buttonContainer: {
     flex: 0.15,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 30,
     backgroundColor: 'transparent',
     flexDirection: 'row',
     justifyContent: 'space-evenly',
